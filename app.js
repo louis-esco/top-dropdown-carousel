9d842ae3a7d5b7aa3c34997de7dd2192b7cec3fb
louis-esco <louis.escoffier.bagrel@gmail.com>
2024-05-04 12:42:44 +0300

    Toggles basic dropdown

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log("Hi");
+const dropdownBtn = document.querySelector(".dropdown-btn");
+const dropdownContent = document.querySelector(".dropdown-content");
+
+function toggleDropdown() {
+  dropdownContent.classList.toggle("show");
+}
+
+dropdownBtn.addEventListener("click", (e) => {
+  e.stopPropagation();
+  toggleDropdown();
+});
+
+document.addEventListener("click", (e) => {
+  if (
+    !dropdownContent.contains(e.target) &&
+    dropdownContent.classList.contains("show")
+  ) {
+    toggleDropdown();
+  }
+});
